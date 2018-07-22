154820b624754aa1804c784bbba81e16d2f3d46f
MarketaHalikova <marketa.halikova@seznam.cz>
2018-07-22 11:17:07 +0200

    Created repositories

---
+++ src/main/java/com/marketahalikova/fopengineweb/repositories/ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.marketahalikova.fopengineweb.repositories;
+
+import com.marketahalikova.fopengineweb.model.Project;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+}
