f31d76af35ab94a022af3e309255fd3ea4bb853c
MarketaHalikova <marketa.halikova@seznam.cz>
2018-08-29 09:42:13 +0200

    Interface methods set up

--- src/main/java/com/marketahalikova/fopengineweb/services/ProjectService.java
+++ src/main/java/com/marketahalikova/fopengineweb/services/ProjectService.java
@@ -1,15 +1,20 @@
 package com.marketahalikova.fopengineweb.services;
 
 import com.marketahalikova.fopengineweb.commands.ProjectDTO;
+import com.marketahalikova.fopengineweb.exceptions.FopEngineException;
+import com.marketahalikova.fopengineweb.exceptions.GitException;
+import com.marketahalikova.fopengineweb.exceptions.XmlException;
 import com.marketahalikova.fopengineweb.model.Project;
 
 import java.util.Set;
 
 public interface ProjectService {
-    Set<Project> getProjects();
-    Project findById(Long l);
-    ProjectDTO saveProjectCommand(ProjectDTO projectDTO);
-    ProjectDTO findCommandById (Long l);
-    Project saveProject(Project project);
-    void deleteById(Long idToDelete);
+
+    Set<Project> getProjects() throws FopEngineException, GitException;
+    Project getProjectById(Long id) throws FopEngineException, GitException;
+    ProjectDTO getProjectCommandById(Long id) throws FopEngineException, GitException;
+    void deleteProjectById(Long id) throws FopEngineException, GitException;
+    Project updateProject(Project project) throws FopEngineException, GitException, XmlException;
+    Project registerNewProject(ProjectDTO project) throws FopEngineException, XmlException;
+
 }
--- src/main/java/com/marketahalikova/fopengineweb/services/ProjectServiceImpl.java
+++ src/main/java/com/marketahalikova/fopengineweb/services/ProjectServiceImpl.java
@@ -1,75 +1,101 @@
 package com.marketahalikova.fopengineweb.services;
 
 import com.marketahalikova.fopengineweb.commands.ProjectDTO;
-import com.marketahalikova.fopengineweb.exceptions.NotFoundException;
-import com.marketahalikova.fopengineweb.mappers.ProjectMapper;
+import com.marketahalikova.fopengineweb.exceptions.FopEngineException;
+import com.marketahalikova.fopengineweb.exceptions.GitException;
+import com.marketahalikova.fopengineweb.exceptions.XmlException;
 import com.marketahalikova.fopengineweb.model.Project;
-import com.marketahalikova.fopengineweb.repositories.ProjectRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
 @Service
 public class ProjectServiceImpl implements ProjectService{
 
-    private final ProjectRepository projectRepository;
 
+    @Override
+    public Set<Project> getProjects() throws FopEngineException, GitException {
+        return null;
+    }
 
-    public ProjectServiceImpl(ProjectRepository projectRepository) {
-        this.projectRepository = projectRepository;
+    @Override
+    public Project getProjectById(Long id) throws FopEngineException, GitException {
+        return null;
+    }
+
+    @Override
+    public ProjectDTO getProjectCommandById(Long id) throws FopEngineException, GitException {
+        return null;
+    }
+
+    @Override
+    public void deleteProjectById(Long id) throws FopEngineException, GitException {
 
     }
 
     @Override
-    public Set<Project> getProjects() {
-        Set<Project> projects = new HashSet<>();
-        projectRepository.findAll().forEach(projects::add);
-        return projects;
+    public Project updateProject(Project project) throws FopEngineException, GitException, XmlException {
+        return null;
     }
 
     @Override
-    public Project findById(Long l) {
-
-        Optional<Project> projectOptional = projectRepository.findById(l);
-
-        if (!projectOptional.isPresent()) {
-            throw new NotFoundException("Recipe Not Found");
-        }
-
-        return projectOptional.get();
-    }
-
-    @Transactional
-    @Override
-    public ProjectDTO saveProjectCommand(ProjectDTO projectDTO) {
-        Project detachedProject = ProjectMapper.INSTANCE.projectDTOToProject(projectDTO);
-
-        Project savedProject = projectRepository.save(detachedProject);
-        log.debug("Saved ProjectId:" + savedProject.getId());
-        return ProjectMapper.INSTANCE.projectToProjectDTO(savedProject);
-    }
-
-    @Transactional
-    @Override
-    public ProjectDTO findCommandById(Long l) {
-        return ProjectMapper.INSTANCE.projectToProjectDTO(findById(l));
+    public Project registerNewProject(ProjectDTO project) throws FopEngineException, XmlException {
+        return null;
     }
 
 
-    @Override
-    public Project saveProject(Project detachedProject) {
-        Project savedProject = projectRepository.save(detachedProject);
-        log.debug("Saved project id: " + savedProject.getId());
-        return savedProject;
-    }
-
-    @Override
-    public void deleteById(Long idToDelete) {
-        projectRepository.deleteById(idToDelete);
-    }
+//    public ProjectServiceImpl(ProjectRepository projectRepository) {
+//        this.projectRepository = projectRepository;
+//
+//    }
+//
+//    @Override
+//    public Set<Project> getProjects() {
+//        Set<Project> projects = new HashSet<>();
+//        projectRepository.findAll().forEach(projects::add);
+//        return projects;
+//    }
+//
+//    @Override
+//    public Project findById(Long l) {
+//
+//        Optional<Project> projectOptional = projectRepository.findById(l);
+//
+//        if (!projectOptional.isPresent()) {
+//            throw new NotFoundException("Recipe Not Found");
+//        }
+//
+//        return projectOptional.get();
+//    }
+//
+//    @Transactional
+//    @Override
+//    public ProjectDTO saveProjectCommand(ProjectDTO projectDTO) {
+//        Project detachedProject = ProjectMapper.INSTANCE.projectDTOToProject(projectDTO);
+//
+//        Project savedProject = projectRepository.save(detachedProject);
+//        log.debug("Saved ProjectId:" + savedProject.getId());
+//        return ProjectMapper.INSTANCE.projectToProjectDTO(savedProject);
+//    }
+//
+//    @Transactional
+//    @Override
+//    public ProjectDTO findCommandById(Long l) {
+//        return ProjectMapper.INSTANCE.projectToProjectDTO(findById(l));
+//    }
+//
+//
+//    @Override
+//    public Project saveProject(Project detachedProject) {
+//        Project savedProject = projectRepository.save(detachedProject);
+//        log.debug("Saved project id: " + savedProject.getId());
+//        return savedProject;
+//    }
+//
+//    @Override
+//    public void deleteById(Long idToDelete) {
+//        projectRepository.deleteById(idToDelete);
+//    }
 }
